d0d8c06ce4885e2be20033b338128ccbd9814103
Danillo Aledes <danilloaledes@gmail.com>
2021-06-09 13:13:19 -0300

    listando empresas

--- src/constants/config.js
+++
@@ -1,4 +0,0 @@
-export const API = "https://backspirax.herokuapp.com"
-const config = { API }
-
-export default config
--- src/pages/Empresa/index.js
+++ src/pages/Empresa/index.js
@@ -1,80 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Lista from '../../components/Lista'
 
-import axios from 'axios'
-import { API } from '../../constants/config'
+import api from '../../services/api';
 
 const Empresa = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get(`${API}/company`).then(response => {
-      console.log(response.data);
-
-      const listaEmpresas = []
-      
-      for (let i = 0; i < response.data.length; i++) {
-        
-        listaEmpresas.push([
-          {
-            _id: response.data[i]._id,
-            sxs_company_name: response.data[i].sxs_company_name,
-            sxs_company_country: response.data[i].sxs_company_country
-          }
-        ])
-      }
-
-      setData(listaEmpresas)
-    })
+    ListaEmpresa();
   }, []);
 
-  /* const data = [
-    {
-      _id: 1,
-      sxs_company_name: "Empresa 1",
-      sxs_company_country: "Estados Un_idos"
-    },
-    {
-      _id: 2,
-      sxs_company_name: "Empresa 2",
-      sxs_company_country: "Itália"
-    },
-    {
-      _id: 3,
-      sxs_company_name: "Empresa 3",
-      sxs_company_country: "Alemanha"
-    },
-    {
-      _id: 4,
-      sxs_company_name: "Empresa 4",
-      sxs_company_country: "Japão"
-    },
-    {
-      _id: 5,
-      sxs_company_name: "Empresa 5",
-      sxs_company_country: "Jamaica"
-    },
-    {
-      _id: 6,
-      sxs_company_name: "Empresa 6",
-      sxs_company_country: "Austrália"
-    },
-    {
-      _id: 7,
-      sxs_company_name: "Empresa 7",
-      sxs_company_country: "Brasil"
-    },
-    {
-      _id: 8,
-      sxs_company_name: "Empresa 8",
-      sxs_company_country: "Grécia"
-    },
-    {
-      _id: 9,
-      sxs_company_name: "Empresa 9",
-      sxs_company_country: "Israel"
+  async function ListaEmpresa() {
+    const response = await api.get('company');
+
+    const empresas = [];
+    for (let i = 0; i < response.data.length; i++) {
+      empresas.push({
+        _id: response.data[i]._id,
+        sxs_company_name: response.data[i].sxs_company_name,
+        sxs_company_country: response.data[i].sxs_company_country
+      });
     }
-  ] */
+
+    setData(empresas);
+  }
 
   return (
     <Lista data={data} categoria="empresa" />
